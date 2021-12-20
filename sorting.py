e4f78cbabb586ff2fd870f8ff842516e4790ff0f
Luca Denhez <lucadenhez@icloud.com>
2021-12-20 00:37:35 -0800

    Re-did old code for mobile, made more aesthetic 😩

--- sorting.py
+++
@@ -1,7 +0,0 @@
-import os
-
-path = "C:/Users/lucad/Documents/LofiWebsite/music"
-files = os.listdir(path)
-
-for i in range (len(files)):
-    os.rename(path + '/' + files[i], path + '/' + str(i) + ".mp3")
